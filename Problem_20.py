d87f614d595bb714649e67995fb47bcd977f9299
YOGEESH K P <yogeesh793@gmail.com>
2025-01-08 13:37:22 +0530

    Add files via upload

---
+++ Problem_20.py
@@ -0,0 +1,11 @@
+"""Odd Numbers Printer:
+
+Create a program that prints all odd numbers between 1 and 20 using a while loop."""
+
+for i in range(0,21):               
+    while i%2!=0:
+        print(i)
+        i=i+1
+
+        
+
